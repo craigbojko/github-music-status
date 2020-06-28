fdeb0e1e5d51da37f1340bf30f7c94118208b2e1
Craig Bojko <craig@pixelventures.co.uk>
2020-06-28 22:22:07 +0100

    chore: top level files

---
+++ README.md
@@ -0,0 +1,1 @@
+# Github Music Status
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+###
+# Project: github-music-status
+# FilePath: /setup.py
+# File: setup.py
+# Created Date: Sunday, June 28th 2020, 10:20:19 pm
+# Author: Craig Bojko (craig@pixelventures.co.uk)
+# -----
+# Last Modified: Sun Jun 28 2020
+# Modified By: Craig Bojko
+# -----
+# Copyright (c) 2020 Pixel Ventures Ltd.
+# ------------------------------------
+# <<licensetext>>
+###
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+# with open('LICENSE') as f:
+#     license = f.read()
+
+setup(
+    name='github-music-status',
+    version='1.0.0',
+    description='',
+    long_description=readme,
+    author='Pixel Ventures Ltd.',
+    author_email='craig@pixelventures.co.uk',
+    url='https://github.com/pixelventures/github-music-status',
+    # license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
